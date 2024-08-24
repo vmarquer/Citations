5a36eb1f2c67919808f02baa21d3e799a3366b05
vmarquer <victor.marquer72@gmail.com>
2024-08-24 18:08:32 +0200

    fix image

--- src/utils/image.tsx
+++ src/utils/image.tsx
@@ -2,7 +2,7 @@ export const getImage = (name: string | undefined, width: string, height: string
     const imageStyle = { width: width, height: height };
 
     if (name) {
-        return <img src={`/img/${name}`} alt={`${name}`} style={imageStyle} />;
+        return <img src={`${process.env.PUBLIC_URL}/img/${name}`} alt={`${name}`} style={imageStyle} />;
     } else {
         return <div>No image</div>;
     }
